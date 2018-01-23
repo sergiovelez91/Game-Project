ce0ef26e002080bae682d912da67c17d40aa5397
Sergio Vélez <sergiovelez.svg@gmail.com>
2018-01-23 09:17:11 +0100

    Actualizacion de questionsDB

--- js/questionsDB.js
+++ js/questionsDB.js
@@ -1,22 +1,80 @@
 var questions = [
     {
         question:"Eres programador?",
-        answers:["si","no"]
+        answers:["si","no"],
+        correctAnswer: "si"
     },
-    {   question:"Hoy es Lunes?",
-        answers: ["si","no"]
+    {   question:"EL año tiene 51 semanas",
+        answers: ["si","no"],
+        correctAnswer: "no"
     },
     {
         question: "Michael Jackson sigue vivo",
-        answers:["si","no"]
+        answers:["si","no"],
+        correctAnswer: "no"
     },
     {
         question: "Rojo y Amarillo = Verde",
-        answers: ["si","no"]
+        answers: ["si","no"],
+        correctAnswer: "no",
+        
     },
     {
-        question: "Real Madrid campeón de liga 16-17",
-        answers: ["si","no"]
+        question: "Real Madrid fue campeón de liga 16-17",
+        answers: ["si","no"],
+        correctAnswer: "si"
+    },
+    {
+        question: "Un Byte es igual a 8 bits",
+        answers: ["si","no"],
+        correctAnswer: "si"
+    },
+    {
+        question: "España es la capital de Madrid",
+        answers: ["si","no"],
+        correctAnswer: "no"
+    },
+    {
+        question: "Al final de la peli siempre ganan los malos",
+        answers: ["si","no"],
+        correctAnswer: "no"
+    },
+    {
+        question: "Tim Berners Lee es el padre de la WEB",
+        answers: ["si","no"],
+        correctAnswer: "si"
+    },
+    {
+        question: "HTML es un lenguaje de programación conocido",
+        answers: ["si","no"],
+        correctAnswer: "no"
+    },
+    {
+        question: "si a 2 le sumas su triple y le restas su mitad su valor es 7",
+        answers: ["si","no"],
+        correctAnswer: "si"
+    },
+    {
+        question: "El animal terrestre mas rápido del mundo es el guepardo",
+        answers: ["si","no"],
+        correctAnswer: "si"
+    },
+    {
+        question: 'Edsger Dijkstra dijo: "Si la depuración es el proceso de eliminar errores,entonces la programación debe ser el proceso de introducirlos"',
+        answers: ["si","no"],
+        correctAnswer: "si"
+    },
+    {
+        question: "Carmen de Mairena era un maromo",
+        answers: ["si","no"],
+        correctAnswer: "si"
+    },
+    {
+        question: "El máximo goleador de la temporada 86/87 murió 10 años más tarde por comer unos mejillones en mal estado",
+        answers: ["si","no"],
+        correctAnswer: "si"
     }
+
+
 ];
 
