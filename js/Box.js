614c0b722252b8e66d5ca551254f9e507a760b84
Sergio Vélez <sergiovelez.svg@gmail.com>
2018-01-24 12:40:29 +0100

    actualización

--- js/Box.js
+++ js/Box.js
@@ -1,13 +1,45 @@
-
 function Box() {
   var index = Math.floor(Math.random() * questions.length);
   this.question = questions[index];
-  console.log(this);
-  var pregunta = $('<div>').text(this.question.text);
-  $("#board").append(pregunta);
+  console.log(this.question);
+  var pregunta = $("<div>").text(this.question.text);
+  // $("#board").append(pregunta);
+  $(".preguntas").text(this.question.text);
   
 }
 
+$(document).keydown(function(e) {
+  if (e.keyCode == 83 && question.positive_questions[index]) {  
+    return true;
+  }
+  else if (e.keyCode == 78 && question.negative_questions[index]) {
+    return true;
+  }
+  else {
+    alert("Game Over")
+  }
+});
+
+
+
+
+$("body").keydown(function(e){
+  switch(e.keyCode){
+      case 83: // YES
+        arr.forEach(function(c){
+          
+        })
+      break;
+      case 78: // NO
+        arr.forEach(function(c){
+          
+        })
+      break;
+  }
+});
+
+
+  
 
 Box.prototype.checkAnswer = function(answer) {
   if (answer === this.question.correctAnswer) {
@@ -16,3 +48,6 @@ Box.prototype.checkAnswer = function(answer) {
     console.log("Has fallado");
   }
 };
+
+// keycode S = 83
+// keycode N = 78
--- js/Game.js
+++ js/Game.js
@@ -1,10 +1,7 @@
-function Game(){
-    this.preguntas = [];
-    
-    for(var i = 0; i< 10;i++){
-        this.preguntas.push(new Box());
-    }
+function Game() {
+  this.preguntas = [];
 
-    
+  for (var i = 0; i < 10; i++) {
+    this.preguntas.push(new Box());
+  }
 }
-
